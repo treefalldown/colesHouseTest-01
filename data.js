cb29e2f9e35ab023af0c99b077fe0abd99b1dcd8
treefalldown <64744535+treefalldown@users.noreply.github.com>
2024-03-24 12:27:52 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,223 @@
+var objInfo = {
+     "image01": {
+        "imageSrc": "01",
+        "latitutde": "42.925313967504756",
+        "longitude": "-78.84711827560466",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+// MAKE SURE TO SPACE THE OBJECTS OUT WITH A COMMA
+    "image02": {
+        "imageSrc": "02",
+        "latitutde": "42.92531281428644",
+        "longitude": "-78.84709574807484",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image03": {
+        "imageSrc": "03",
+        "latitutde": "42.925321400800065",
+        "longitude": "-78.84690030919826",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image04": {
+        "imageSrc": "04",
+        "latitutde": "42.925420930296426",
+        "longitude": "-78.84696147467852",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image05": {
+        "imageSrc": "05",
+        "latitutde": "42.92508540058955",
+        "longitude": "-78.8474856255901",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image06": {
+        "imageSrc": "06",
+        "latitutde": "42.92514333955289",
+        "longitude": "-78.8473039059317",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image07": {
+        "imageSrc": "07",
+        "latitutde": "42.92514871623745",
+        "longitude": "-78.84725861380612",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image08": {
+        "imageSrc": "08",
+        "latitutde": "42.92519069484946",
+        "longitude": "-78.84725340210285",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image09": {
+        "imageSrc": "09",
+        "latitutde": "42.925095220757434",
+        "longitude": "-78.84728714212609",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image10": {
+        "imageSrc": "10",
+        "latitutde": "42.92518998528835",
+        "longitude": "-78.84733341023006",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image11": {
+        "imageSrc": "11",
+        "latitutde": "42.925415257448385",
+        "longitude": "-78.84701415593267",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image12": {
+        "imageSrc": "12",
+        "latitutde": "42.925284192565265",
+        "longitude": "-78.84689249164336",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image13": {
+        "imageSrc": "13",
+        "latitutde": "42.92521263820344",
+        "longitude": "-78.8472091026251",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image14": {
+        "imageSrc": "14",
+        "latitutde": "42.9252374437246",
+        "longitude": "-78.8472012850702",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image15": {
+        "imageSrc": "15",
+        "latitutde": "42.925224086906745",
+        "longitude": "-78.84717783240552",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image16": {
+        "imageSrc": "16",
+        "latitutde": "42.925571827862605",
+        "longitude": "-78.84721713370595",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image17": {
+        "imageSrc": "17",
+        "latitutde": "42.92499898305283",
+        "longitude": "-78.84742125257661",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image18": {
+        "imageSrc": "18",
+        "latitutde": "42.92512075318419",
+        "longitude": "-78.84730591758836",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image19": {
+        "imageSrc": "19",
+        "latitutde": "42.92549921554608",
+        "longitude": "-78.84717529859238",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    
+    "image20": {
+        "imageSrc": "20",
+        "latitutde": "42.92526129517885",
+        "longitude": "-78.84679607513296",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image21": {
+        "imageSrc": "21",
+        "latitutde": "42.92514972265582",
+        "longitude": "-78.84738571330352",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+    "image22": {
+        "imageSrc": "22",
+        "latitutde": "42.925134992418215",
+        "longitude": "-78.84727037831706",
+        "rotation": "0 0 0",
+        "scale": "10 10 10",
+        "animationFrom": "0.9",
+        "animationTo": "0.4"
+     },
+    
+};
